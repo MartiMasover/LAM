045696802cec97e99ba30d5f1caeb78508eadc12
Alex Muñoz <amvx2712@gmail.com>
2022-01-21 10:43:41 +0100

    COMMIT alex

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello DAM1");
         System.out.println("Martí Sans Lleixà");
+        System.out.println("Alejandro Muñoz");
     }
 }
